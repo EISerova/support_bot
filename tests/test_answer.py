3e95ddcb247d6478baa6a08c130613645b54d0c2
Екатерина Серова <katyaserova@yandex.ru>
2022-09-19 20:15:49 +0300

    add tests

---
+++ tests/test_answer.py
@@ -0,0 +1,13 @@
+import pytest
+from unittest.mock import AsyncMock
+from handlers.echo import bot_echo
+from settings.constants import ECHO_ANSWER
+
+
+
+@pytest.mark.asyncio
+async def test_echo_handler():
+    text_mock = ECHO_ANSWER
+    message_mock = AsyncMock(text=text_mock)
+    await bot_echo(message=message_mock)
+    message_mock.answer.assert_called_with(text_mock)
