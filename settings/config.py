3799d1aecaca21e449a4aaa2b88688db0478e6f4
Екатерина Серова <katyaserova@yandex.ru>
2022-09-25 17:53:19 +0300

    add logging

--- settings/config.py
+++ settings/config.py
@@ -11,10 +11,10 @@ try:
     BOT_TOKEN = env.str("BOT_TOKEN")
     ADMINS = env.list("ADMINS")
     FIRST_OPERATOR = env.str("FIRST_OPERATOR")
-    SECOND_OPERATOR = env.str("SECOND_OPERATOR")
 except EnvError as error:
     logging.critical(ERROR_TOKEN.format(error=error), exc_info=True)
     exit(1)
 
+SECOND_OPERATOR = env.str("SECOND_OPERATOR")
 OPERATOR_IDS = [FIRST_OPERATOR, SECOND_OPERATOR]
 OPERATOR_LOGINS = {FIRST_OPERATOR: "FIRST_OPERATOR", SECOND_OPERATOR: "SECOND_OPERATOR"}
