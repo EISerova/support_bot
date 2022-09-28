214859507d498259c680a1fc0b493631ddd7d053
Екатерина Серова <katyaserova@yandex.ru>
2022-09-28 20:14:25 +0300

    fix middleware

--- middlewares/support_middleware.py
+++ middlewares/support_middleware.py
@@ -1,28 +1,3 @@
-# import logging
-
-# from aiogram import types
-# from aiogram.dispatcher.handler import CancelHandler
-# from aiogram.dispatcher.middlewares import BaseMiddleware
-
-# from loader import dp
-
-
-# class SupportMiddleware(BaseMiddleware):
-#     async def on_pre_process_message(self, message: types.Message, data: dict):
-#         state = dp.current_state(chat=message.from_user.id, user=message.from_user.id)
-#         state_str = str(await state.get_state())
-#         if state_str == "in_support":
-#             data = await state.get_data()
-#             operator_id = data.get("user_id")
-
-#             try:
-#                 await message.copy_to(operator_id)
-#             except Exception as error:
-#                 logging.exception(error)
-
-#             raise CancelHandler()
-
-
 import logging
 
 from aiogram import types
@@ -32,23 +7,17 @@ from aiogram.dispatcher.middlewares import BaseMiddleware
 from loader import dp
 
 
-# Создадим миддлварь, в котором полностью будет  проходить обработка сообщений
-# для пользователя и операторов, которые находятся на связи.
-# Отсюда сообщения в хендлеры даже направляться не будут
 class SupportMiddleware(BaseMiddleware):
     async def on_pre_process_message(self, message: types.Message, data: dict):
-        # Для начала достанем состояние текущего пользователя,
-        # так как state: FSMContext нам сюда не прилетит
         state = dp.current_state(chat=message.from_user.id, user=message.from_user.id)
-
-        # Получим строчное значение стейта и сравним его
         state_str = str(await state.get_state())
         if state_str == "in_support":
-
-            # Заберем айди второго пользователя и отправим ему сообщение
             data = await state.get_data()
-            second_id = data.get("second_id")
-            await message.copy_to(second_id)
+            operator_id = data.get("second_id")
 
-            # Не пропустим дальше обработку в хендлеры
+            try:
+                await message.copy_to(operator_id)
+            except Exception as error:
+                logging.exception(error)
+
             raise CancelHandler()
